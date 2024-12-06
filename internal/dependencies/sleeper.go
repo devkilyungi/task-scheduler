a9c31f40bd644f41edde18b67b1f2ee5ef2cadfa
Victor Kilyungi <victorkilyungi@gmail.com>
2024-12-06 14:13:19 +0300

    create sleeper interface and test configurable sleeper struct

--- internal/dependencies/sleeper.go
+++ internal/dependencies/sleeper.go
@@ -1,1 +1,16 @@
 package dependencies
+
+import "time"
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleepFn  func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleepFn(c.duration)
+}
--- internal/dependencies/sleeper_test.go
+++ internal/dependencies/sleeper_test.go
@@ -1,1 +1,26 @@
 package dependencies
+
+import (
+	"testing"
+	"time"
+)
+
+type SpySleeper struct {
+	durationSlept time.Duration
+}
+
+func (s *SpySleeper) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 3 * time.Second
+
+	spySleeper := &SpySleeper{}
+	sleeper := ConfigurableSleeper{sleepTime, spySleeper.Sleep}
+	sleeper.Sleep()
+
+	if spySleeper.durationSlept != sleepTime {
+		t.Fatalf("should have slept for %v, but slept for %v", sleepTime, spySleeper.durationSlept)
+	}
+}
